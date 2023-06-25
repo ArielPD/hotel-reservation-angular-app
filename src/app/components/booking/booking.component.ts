f05146b2f7c092bb9fd50a1a9db9f107d2c08277
Ariel Pinto <arielpinto1041@gmail.com>
2023-06-24 23:28:02 -0400

    Add form booking and add selector form

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  //property
+  countries: any;
+  formGroup: FormGroup;
+
+  constructor(private countriesService:CountriesService) { 
+    this.formGroup = new FormGroup({
+      email: new FormControl(null),
+      customerName: new FormControl(null),
+      country: new FormControl(null)
+    });
+  }
 
   ngOnInit(): void {
+    this.countriesService.getCountries().subscribe(
+      (response) => {
+        this.countries = response;
+      }, (error) => {
+        console.log(error);
+      })
   }
 
 }
